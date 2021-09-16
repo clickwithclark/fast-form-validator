3615c4b38d0def57b3e6833b8f3b5bc17cc3a6ca
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2021-09-16 17:18:55 -0400

    squash! WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/index.js'],
-    output: {
-      filename: 'ffv.min.js',
-      path: path.resolve(__dirname, 'UMD'),
-     
+  entry: ['./src/index.js'],
+  output: {
+    filename: 'ffv.min.js',
+    path: path.resolve(__dirname, 'UMD'),
+
     library: {
       type: 'umd',
     },
     // prevent error: `Uncaught ReferenceError: self is not define`
     // globalObject: 'this',
-    },
-    mode: 'development',
-
-  };
-  
+  },
+  mode: 'production',
+};
