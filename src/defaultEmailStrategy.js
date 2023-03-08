47f8f24c6b6e7ea2b7836f47b3811bf462e73c06
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2023-03-08 13:46:25 -0400

    fix:email id's can now be anything vs only 'email'

--- src/defaultEmailStrategy.js
+++ src/defaultEmailStrategy.js
@@ -1,12 +1,21 @@
-import { getDefaults } from './stateManagement.js';
+import { getFormInputs, getFormState } from './stateManagement.js';
 
-export function defaultEmailStrategy() {
+export function defaultEmailStrategy(id) {
   // eslint-disable-next-line no-useless-escape
   const validEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!this.emailValue) {
-    this.emailError = '❌Email cannot be empty';
+  console.log('formInputsArr', getFormInputs());
+  console.log('formState', getFormState());
+  console.log('this[${id}Value]', this[`${id}Value`]);
+  console.log('email1Value', this.email1Value);
+  console.log({ FFV: this });
+
+  console.log('Pretty\n', new Error().stack.split('\n'));
+  console.log('Ugly\n', new Error().stack);
+
+  if (!this[`${id}Value`]) {
+    this[`${id}Error`] = '❌Email cannot be empty';
   }
-  if (this.emailValue && !validEmailRegex.test(this.emailValue)) {
-    this.emailError = '❌Email must be valid';
+  if (this[`${id}Value`] && !validEmailRegex.test(this[`${id}Value`])) {
+    this[`${id}Error`] = '❌Email must be valid';
   }
 }
