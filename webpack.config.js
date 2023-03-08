fed03a385846e382150303b8f4a324e98a9e0cea
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2023-03-08 13:36:50 -0400

    feat:add terser-webpack-plugin
    The plugin is used to make to make
    troubleshooting easier by keeping
    function names

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: ['./src/index.js'],
@@ -16,4 +17,15 @@ module.exports = {
   // mode: 'development',
   watch: true,
   // devtool:'hidden-cheap-module-source-map'
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       terserOptions: {
+  //         keep_classnames: true,
+  //         keep_fnames: true,
+  //       },
+  //     }),
+  //   ],
+  // },
 };
